0986cb124fd5d320045d2ff78bbf639ff6dece62
Matheus Martino <matheus.martino97@gmail.com>
2021-06-03 19:50:01 -0300

    Updates

---
+++ precache-manifest.ddade7c1dee6ddc2c1ec2c4d67bb3a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df1bd572471bf5236743a6f673d8228",
+    "url": "/21_sd-09-project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "af869313b25bd3e844fb",
+    "url": "/21_sd-09-project-starwars-planets-search/static/css/main.9192596c.chunk.css"
+  },
+  {
+    "revision": "da74d0854bb37f552e2b",
+    "url": "/21_sd-09-project-starwars-planets-search/static/js/2.f6837d08.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/21_sd-09-project-starwars-planets-search/static/js/2.f6837d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af869313b25bd3e844fb",
+    "url": "/21_sd-09-project-starwars-planets-search/static/js/main.1698d59f.chunk.js"
+  },
+  {
+    "revision": "7df6b3a5a49686cced11",
+    "url": "/21_sd-09-project-starwars-planets-search/static/js/runtime-main.0b9ea136.js"
+  }
+]);
